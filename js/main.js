8d1a5fdae11855cdec85bc82ccb53d2379008eec
Matthew Powell <matt.powell2784@gmail.com>
2022-11-20 21:19:22 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,83 @@
+'use strict'
+import { projectDetails } from './projectDetails.js'
+
+const toogleCardDetails = (e) => {
+  const buttonClass = e.target.id
+  const key = buttonClass.slice(-1)
+  const h1 = document.getElementById(`card__h1_${key}`)
+  const description = projectDetails[key].description
+  const descriptionVisible = projectDetails[key].state.descriptionVisible
+
+  if (!descriptionVisible) {
+    //add p node
+    const p = document.createElement('p')
+    p.classList.add('card__p')
+    p.setAttribute('id', `card__p_${key}`)
+    const pText = document.createTextNode(description)
+    p.appendChild(pText)
+    h1.append(p)
+
+    //change button text
+    const button = document.getElementById(buttonClass)
+    button.textContent = 'Hide Details'
+
+    //set text state
+    projectDetails[key].state.descriptionVisible = true
+  }
+
+  if (descriptionVisible) {
+    //remove p node
+    const p = document.getElementById(`card__p_${key}`)
+    p.remove()
+
+    //change button text
+    const button = document.getElementById(buttonClass)
+    button.textContent = 'View Details'
+
+    //set text state
+    projectDetails[key].state.descriptionVisible = false
+  }
+}
+
+projectDetails.forEach((project) => {
+  //create card node
+  const card = document.createElement('div')
+  card.classList.add('card')
+
+  //add image node
+  const img = document.createElement('img')
+  img.classList.add('card__img')
+  img.setAttribute('src', project.image)
+  img.setAttribute('alt', 'Flower')
+  card.appendChild(img)
+
+  //add h1 node
+  const h1 = document.createElement('h1')
+  h1.classList.add('card__h1')
+  h1.setAttribute('id', `card__h1_${project.key}`)
+  const h1Text = document.createTextNode(project.title)
+  h1.appendChild(h1Text)
+  card.appendChild(h1)
+
+  //add button container node
+  const buttonContainer = document.createElement('div')
+  buttonContainer.classList.add('button__container')
+  card.appendChild(buttonContainer)
+
+  //add button node
+  const button = document.createElement('button')
+  button.classList.add('card__button')
+  const buttonText = document.createTextNode('View Details')
+  button.setAttribute('id', `card_button_${project.key}`)
+  button.appendChild(buttonText)
+  buttonContainer.appendChild(button)
+
+  //insert nodes into DOM
+  const cardContainer = document.getElementById('card__container')
+  cardContainer.appendChild(card)
+
+  //button event listener
+  button.addEventListener('click', toogleCardDetails)
+})
+
+//addProjectCardNodes()
---
+++ js/projectDetails.js
@@ -0,0 +1,38 @@
+export const projectDetails = [
+  {
+    key: 0,
+    image: '/img/ots_grass.jpg',
+    title: 'Old Thorntonians FC Player Registration App',
+    description:
+      'Frontend built with React, TypeScript, Redux Toolkit and Styled Components. Backend built with NodeJS, Express, Mongo DB, Express Validator, Bcrypt and JWT.',
+    href: 'https://otsregister.netlify.app/',
+    state: { descriptionVisible: false },
+  },
+  {
+    key: 1,
+    image: '/img/scar_bg.jpg',
+    title: 'Guess The Film Year App',
+    description:
+      'Guess the film year game created using React, Styled Components and Redux. I used this project to gain a better understanding of the basics of React.',
+    href: 'https://guessthefilmyear.netlify.app',
+    state: { descriptionVisible: false },
+  },
+  {
+    key: 2,
+    image: '/img/weather_clouds.jpg',
+    title: 'Weather App',
+    description:
+      "A weather app made using vanilla JavaScript, HTML and CSS.I used this project to practice my newly learnt JavaScript skills, it uses async functions, geocoding API's, utilizes DOM manipulation and CSS media queries for a responsive design.",
+    href: 'https://mattpowell2784-javascript-weather-app.netlify.app/',
+    state: { descriptionVisible: false },
+  },
+  {
+    key: 3,
+    image: '/img/projects_small.jpg',
+    title: 'React Portfolio Page',
+    description:
+      'My portfolio site built with React. This site utilises React Context, Styled Components Theme Provider, CSS animations, a responsive layout and emailJS library to provide form functionality without a backend.',
+    href: 'https://mattpowell2784-javascript-weather-app.netlify.app/',
+    state: { descriptionVisible: false },
+  },
+]
