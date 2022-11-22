fae23929b458c2ddb3f041672500167bcd7cfc32
Matthew Powell <matt.powell2784@gmail.com>
2022-11-22 21:20:30 +0000

    image paths updated

--- js/projectDetails.js
+++ js/projectDetails.js
@@ -1,7 +1,7 @@
 export const projectDetails = [
   {
     key: 0,
-    image: '/img/ots_grass.jpg',
+    image: './img/ots_grass.jpg',
     title: 'Old Thorntonians FC Player Registration App',
     description:
       'Frontend built with React, TypeScript, Redux Toolkit and Styled Components. Backend built with NodeJS, Express, Mongo DB, Express Validator, Bcrypt and JWT.',
@@ -11,7 +11,7 @@ export const projectDetails = [
   },
   {
     key: 1,
-    image: '/img/scar_bg.jpg',
+    image: './img/scar_bg.jpg',
     title: 'Guess The Film Year App',
     description:
       'Guess the film year game created using React, Styled Components and Redux. I used this project to gain a better understanding of the basics of React.',
@@ -21,7 +21,7 @@ export const projectDetails = [
   },
   {
     key: 2,
-    image: '/img/weather_clouds.jpg',
+    image: './img/weather_clouds.jpg',
     title: 'Weather App',
     description:
       "A weather app made using vanilla JavaScript, HTML and CSS.I used this project to practice my newly learnt JavaScript skills, it uses async functions, geocoding API's, utilizes DOM manipulation and CSS media queries for a responsive design.",
@@ -31,7 +31,7 @@ export const projectDetails = [
   },
   {
     key: 3,
-    image: '/img/projects_small.jpg',
+    image: './img/projects_small.jpg',
     title: 'React Portfolio Page',
     description:
       'My portfolio site built with React. This site utilises React Context, Styled Components Theme Provider, CSS animations, a responsive layout and emailJS library to provide form functionality without a backend.',
